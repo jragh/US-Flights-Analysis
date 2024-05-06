f9948fd08ba142193ec043cfbf252a3f5a94ffab
jragh <jordanraghunandan@gmail.com>
2024-05-06 02:23:06 -0400

    Related to #1, added in dropdown for passenger visualization selection

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import polars as pl
-from dash import html, dcc, Input, Output
+from dash import html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
 import plotly_express as px
 
@@ -8,6 +8,8 @@ import dash
 sqlite_path = 'sqlite://US_Flights_Analytics.db'
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+
+passenger_viz_list = ['Passengers By Carrier', 'Passenger Utilization By Carrier (%)', 'Passenger Utilization Monthly']
 
 analytics_df = pl.read_database_uri(query='SELECT * FROM T100_SEGMENT_ALL_CARRIER_2023 ORDER BY ROUND(CAST([PASSENGERS] as FLOAT), 2) desc', uri=sqlite_path,engine='adbc')
 
@@ -39,8 +41,43 @@ app.layout = dbc.Container([
                 html.H2('Graphs will go here', style={'textAlign': 'center'}),
                 html.Hr(),
 
-                dbc.Label('Select an Airline Carrier from the dropdown below: '),
-                html.Div([dcc.Dropdown(options=carrier_filter_list, multi=False, searchable=True, clearable=True, placeholder='Select an Airline Carrier here...', id='carrier-selection')], className='dbc mb-2'),
+                dbc.Row([
+                    dbc.Col([
+                        
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    dbc.Label('Airline Carrier Selection:  '),
+                                    html.Div([dcc.Dropdown(options=carrier_filter_list, multi=False, searchable=True, clearable=True, 
+                                                           placeholder='Select an Airline Carrier here...', id='carrier-selection',
+                                                           style={'fontSize': '12px'})], className='dbc mb-2')
+
+                                ], className='pt-3 pb-2'
+                            ), className='bg-light text-dark border rounded-3'
+                        )
+
+                    ], width=12, md=8, id='carrier-selection-col', className='mb-2'),
+
+                    dbc.Col([
+
+                        dbc.Card(
+                            dbc.CardBody([
+                                dbc.Label('Visual Selection: '),
+                                html.Div([dcc.Dropdown(options=passenger_viz_list, value='Passengers By Carrier', multi=False, searchable=True, clearable=False, 
+                                                       placeholder='Select an Data Viz hre...', id='passenger-viz-selection',
+                                                       style={'fontSize': '12px'})], className='dbc mb-2')
+
+                            ], className='pt-3 pb-2'
+                            ), className='bg-light text-dark border rounded-3'
+                        )
+
+                    ], width=12, md=4, id='visual-selection-col', className='mb-2')
+                ]),
+
+                
+
+                
+                
 
                 ## Creating Graph Component for quick analytics ##
                 html.Div([
@@ -66,58 +103,52 @@ app.layout = dbc.Container([
 @app.callback(
     Output(component_id='passengers-graph', component_property='figure'),
     Output(component_id='graph-header', component_property='children'),
-    Input(component_id='carrier-selection', component_property='value')
+    [Input(component_id='carrier-selection', component_property='value'),
+     Input(component_id='passenger-viz-selection', component_property='value')
+    ]
 )
-def passengers_carrier_selection(selected_carrier):
+def passengers_carrier_selection(selected_carrier, selected_pass_viz):
 
-    pass_carrier = pl.read_database_uri(query='SELECT * FROM T100_PASSENGERS_BY_CARRIER_2023', uri=sqlite_path,engine='adbc')
+    months_text = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-    if selected_carrier is None or selected_carrier.strip() == '':
+    ## First Return is Passengers By Carrier ##
+    if selected_pass_viz == 'Passengers By Carrier':
+        
+        pass_carrier = pl.read_database_uri(query='SELECT * FROM T100_PASSENGERS_BY_CARRIER_2023', uri=sqlite_path,engine='adbc')
 
-        bar_figure = px.bar(pass_carrier.select(['MONTH', 'TOTAL PASSENGERS']).group_by(pl.col('MONTH')).sum(), x='MONTH', y='TOTAL PASSENGERS', text_auto='.2s', title='Passengers Transported By Month: All Carriers')
+        if selected_carrier is None or selected_carrier.strip() == '':
 
-        bar_figure.update_traces(textposition='outside', textangle=0)
+            bar_figure = px.bar(pass_carrier.select(['MONTH', 'TOTAL PASSENGERS']).group_by(pl.col('MONTH')).sum(), x='MONTH', y='TOTAL PASSENGERS', text_auto='.2s', title='Passengers Transported By Month: All Carriers')
 
-        bar_figure.update_xaxes(categoryorder='array', categoryarray=[1,2,3,4,5,6,7,8,9,10,11,12])
+            bar_figure.update_traces(textposition='outside', textangle=0)
 
-        return bar_figure, ['All Carriers: Number of Passengers Transported']
+            bar_figure.update_xaxes(categoryorder='array', categoryarray=months_text, linewidth=2.5, showgrid=False, linecolor='rgb(204, 204, 204)')
+
+            bar_figure.update_yaxes( showgrid=True, zeroline=False, showline=False, showticklabels=True, tickwidth=2,gridcolor="rgba(30, 63, 102, 0.15)")
+
+            bar_figure.update_layout(plot_bgcolor='white')
+
+            return bar_figure, ['All Carriers: Number of Passengers Transported']
+        
+        else:
+
+            bar_figure = px.bar(pass_carrier.filter(pl.col('UNIQUE_CARRIER_NAME') == selected_carrier), x='MONTH', y='TOTAL PASSENGERS', text_auto='.2s', title=f'Passengers Transported By Month: {selected_carrier}')
+
+            bar_figure.update_traces(textposition='outside', textangle=0)
+
+            bar_figure.update_xaxes(categoryorder='array', categoryarray=months_text, linewidth=2.5, showgrid=False, linecolor='rgb(204, 204, 204)')
+            
+            bar_figure.update_yaxes( showgrid=True, zeroline=False, showline=False, showticklabels=True, tickwidth=2, gridcolor="rgba(30, 63, 102, 0.15)")
+
+            bar_figure.update_layout(plot_bgcolor='white')
+            
+
+
+            return bar_figure, [f'{selected_carrier}: Number of Passengers Transported']
+
+    ## Second Return is for showing seat usage utilization ##   
     
-    else:
 
-        bar_figure = px.bar(pass_carrier.filter(pl.col('UNIQUE_CARRIER_NAME') == selected_carrier), x='MONTH', y='TOTAL PASSENGERS', text_auto='.2s', title=f'Passengers Transported By Month: {selected_carrier}')
-
-        bar_figure.update_traces(textposition='outside', textangle=0)
-
-        bar_figure.update_xaxes(categoryorder='array', categoryarray=[1,2,3,4,5,6,7,8,9,10,11,12], linewidth=4, showgrid=False, linecolor='rgb(204, 204, 204)')
-        
-        bar_figure.update_yaxes( showgrid=True, zeroline=False, showline=False, showticklabels=True, gridcolor="rgba(30, 63, 102, 0.25)")
-
-        bar_figure.update_layout(plot_bgcolor='white')
-        
-
-
-        return bar_figure, [f'{selected_carrier}: Number of Passengers Transported']
-
-
-
-# xaxis=dict(
-#         showline=True,
-#         showgrid=False,
-#         showticklabels=True,
-#         linecolor='rgb(204, 204, 204)',
-#         linewidth=2,
-#         ticks='outside',
-#         tickfont=dict(
-#             family='Arial',
-#             size=12,
-#             color='rgb(82, 82, 82)',
-#         ),
-#     ),
-#     yaxis=dict(
-#         showgrid=False,
-#         zeroline=False,
-#         showline=False,
-#         showticklabels=False,
 
 
 
