79be6aa86b05050760054d44fb0187a20ac3fbe8
jragh <jordanraghunandan@gmail.com>
2024-06-18 02:44:46 -0400

    #13 Adjusted Home Page styling, title, added animation, next step additional sub animations for examples

--- pages/Home_Page.py
+++ pages/Home_Page.py
@@ -15,27 +15,31 @@ layout = html.Div([
 
                     dbc.Col([
 
-                        html.Span(
+                        html.Div(
                             
-                            [html.H1('HOLY MOLY THIS IS SOME BIG TEXT', className='home-banner-title-text'),
-                            html.P('This is some Smaller Example text')]
+                            [html.H1('Welcome To', className='home-banner-title-text'),
+                             html.Span(html.H1('FLYLYTICS'), className='home-banner-title-italics'),
+                            html.P('An Analytics Dashboard containing information on flights throughout the USA!', style={'color': 'rgb(199, 199, 200)', 'fontWeight': 100})]
                             
                         , style={'display': 'inline-block'}, className='home-page-title-text-box')
 
-                    ],lg=4, md=6, sm=12, xs=12, width=3, className='d-flex align-items-center'),
+                    ],lg=4, md=6, sm=12, xs=12, width=12, className='d-flex align-items-start justify-content-sm-center justify-content-md-start justify-content-center title-animate'),
 
 
                     dbc.Col([
 
                         html.Div([
 
-                            html.H2('Check out some ', id='home-page-header'),
+                            html.H2('An Analytics App on USA Flights Data', id='home-page-header', style={'color': 'rgb(199, 199, 200)'}),
                             html.Hr(className='my-2'),
-                            html.P('Test 1', className='mb-2', id='airports-graph-desc')
+                            html.Small('''Explore this Web app to discover key data relating to US passenger flights.
+                                   Available themes in the app include Passenger, Airport, Carrier, Flight Route, and Flight Delay Data.
+                                   See below for an example on the type of information you can explore: ''', className='mb-2', id='airports-graph-desc',
+                                   style={'color': 'rgb(199, 199, 200)'})
 
                     ], className='p-4 rounded-3 home-page-example-animate-box flex-fill', id='home-visual-div')
 
-                    ],lg=6, md=6, sm=12, xs=12, width=8, className='d-flex align-items-center')
+                    ],lg=6, md=6, sm=12, xs=12, width=8, className='d-flex align-items-start title-animate')
 
 
                 ], style={'height': '100%'}, justify='around')
