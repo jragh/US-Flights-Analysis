c3df4ec55e7798f79dbf6f733edea307f511626f
jragh <jordanraghunandan@gmail.com>
2024-10-21 02:10:58 -0400

    #37 WIP Creating pills and popover tooltips for homepage. Needs testing as well.

--- pages/Home_Page.py
+++ pages/Home_Page.py
@@ -1,6 +1,8 @@
 from dash import html, dcc, Input, Output, State, callback, no_update, register_page
 import dash_bootstrap_components as dbc
 
+from .Home_Page_AG_Grid import home_page_grid
+
 register_page(__name__, path='/Home')
 
 layout = html.Div([
@@ -39,42 +41,80 @@ layout = html.Div([
 
                             html.H6('Top 5 Busiest Trips By Passengers in US (By Passenger Count)', style={'color': '#E89C31'}, className='my-3'),
 
-                            html.Span([
+                            ## First Row will have 3 pills
+                            dbc.Row([
+                                
+                                ## Col for Home Page Pills ##
+                                ## TODO: Create separate component for home page pills to work ##
+                                dbc.Col([
 
-                                html.H6([
-                                    html.Span('#1: LONDON, UK (LHR) AND NEW YORK, NY (JFK)', className='example_head_1'),
+                                    html.Span(html.Small('1: LHR & JFK'), className='mx-1 px-2 py-1', id='home-pill-1',
+                                              style={'backgroundColor': '#62B3E0', 'color': '#0B2838', 'borderRadius': '20px'}),
 
-                                    html.Span('#2: ATLANTA, GA (ATL) AND ORLANDO, FL (MCO)', className='example_head_2'),
+                                    ## Popover for Home Page Pill ##
+                                    dbc.Popover(
+                                        [
+                                            dbc.PopoverHeader('LONDON, UK (LHR) AND NEW YORK, NY (JFK)', style={'fontSize': '80%', 'fontWeight': 'bold'}),
+                                            dbc.PopoverBody(children=[
+                                            html.Span([
+                                                html.Strong('Passengers Flown: '),
+                                                html.P('2,987,159')
+                                                ])
+                                            ], className='px-3 py-2')
+                                        ],
+                                        body=True,
+                                        target='home-pill-1',
+                                        trigger='legacy',
+                                        flip=True
+                                    )
 
-                                    html.Span('#3: LOS ANGELES, CA (LAX) AND NEW YORK, NY (JFK)', className='example_head_3'),
-
-                                    html.Span('#4: LAS VEGAS, NV (LAS) AND LOS ANGELES, CA (LAX)', className='example_head_4'),
-
-                                    html.Span('#5: LOS ANGELES, CA (LAX) AND SAN FRANCISCO, CA (SFO)', className='example_head_5')
-                            ], className='m-0')
-
-                            ], className='busy-trips-header-home-page'),
-
-                            html.Div(style={'height': '1.5em'}),
+                                ], xl = 3, lg = 4, md = 6, sm = 6, xs = 6, style={'textAlign': 'center'})
 
 
-                            html.Span([
+                            ], justify='center', className='mt-2 mb-4'),
 
-                                html.H6([
+                            dbc.Row([]),
 
-                                    html.Span('Passengers: 2 987 159, Flights: 14 634, Servicing Carriers: 5', className='example_head_stats_1'),
+                            dbc.Row([]),
 
-                                    html.Span('Passengers: 2 952 159, Flights: 18 009, Servicing Carriers: 7', className='example_head_stats_2'),
+                            home_page_grid
 
-                                    html.Span('Passengers: 2 812 807, Flights: 19 935, Servicing Carriers: 5', className='example_head_stats_3'),
+                            # html.Span([
 
-                                    html.Span('Passengers: 2 727 943, Flights: 22 440, Servicing Carriers: 20', className='example_head_stats_4'),
+                            #     html.H6([
+                            #         html.Span('#1: LONDON, UK (LHR) AND NEW YORK, NY (JFK)', className='example_head_1'),
 
-                                    html.Span('Passengers: 2 709 771, Flights: 23 368, Servicing Carriers: 13', className='example_head_stats_5')
+                            #         html.Span('#2: ATLANTA, GA (ATL) AND ORLANDO, FL (MCO)', className='example_head_2'),
 
-                                ])
+                            #         html.Span('#3: LOS ANGELES, CA (LAX) AND NEW YORK, NY (JFK)', className='example_head_3'),
 
-                            ], className='busy-trips-header-home-page-stats')
+                            #         html.Span('#4: LAS VEGAS, NV (LAS) AND LOS ANGELES, CA (LAX)', className='example_head_4'),
+
+                            #         html.Span('#5: LOS ANGELES, CA (LAX) AND SAN FRANCISCO, CA (SFO)', className='example_head_5')
+                            # ], className='m-0')
+
+                            # ], className='busy-trips-header-home-page'),
+
+                            # html.Div(style={'height': '1.5em'}),
+
+
+                            # html.Span([
+
+                            #     html.H6([
+
+                            #         html.Span('Passengers: 2 987 159, Flights: 14 634, Servicing Carriers: 5', className='example_head_stats_1'),
+
+                            #         html.Span('Passengers: 2 952 159, Flights: 18 009, Servicing Carriers: 7', className='example_head_stats_2'),
+
+                            #         html.Span('Passengers: 2 812 807, Flights: 19 935, Servicing Carriers: 5', className='example_head_stats_3'),
+
+                            #         html.Span('Passengers: 2 727 943, Flights: 22 440, Servicing Carriers: 20', className='example_head_stats_4'),
+
+                            #         html.Span('Passengers: 2 709 771, Flights: 23 368, Servicing Carriers: 13', className='example_head_stats_5')
+
+                            #     ])
+
+                            # ], className='busy-trips-header-home-page-stats')
                             
 
                     ], className='p-4 rounded-3 home-page-example-animate-box flex-fill', id='home-visual-div')
